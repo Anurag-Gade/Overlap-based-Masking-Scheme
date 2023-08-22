ed1dd317288f282ebadcfce391bd0efe34e0785b
Anurag Gade <88252632+Anurag-Gade@users.noreply.github.com>
2023-08-22 14:13:28 -0400

    Added binary masking

--- src/main.py
+++ src/main.py
@@ -5,14 +5,12 @@ from conv2dict import *
 from adjacency_check import *
 from create_overlapped_cuboid import *
 
-# Replace 'cuboid_data' with the actual 3D cuboid data (a 3D NumPy array)
-cuboid_data = np.random.rand(132,132,90)  # Replace this with your cuboid data
-# total_num_sub_cuboids = 12  # Replace with the total number of sub-cuboids you want
-overlap = 10  # Replace with the desired overlap (integer)
-# num_sub_cubes_depth = 2  # Replace with the number of sub-cubes along the depth dimension (integer)
-num_sub_cuboids_dim = {'num_sub_cuboids_x':10,
-                      'num_sub_cuboids_y':10,
-                      'num_sub_cuboids_z':8}
+cuboid_data = np.random.rand(145, 174, 145)  # Replace this with your cuboid data
+# total_num_sub_cuboids = 4  # Replace with the total number of sub-cuboids you want
+overlap = 8  # Replace with the desired overlap (integer)
+num_sub_cuboids_dim = {"num_sub_cuboids_x":5,  # Replace with the number of sub-cubes along the depth dimension (integer)
+                   "num_sub_cuboids_y":6,
+                   "num_sub_cuboids_z":5}
 
 
 sub_cuboids, start_coords, end_coords = divide_cuboid(cuboid_data, overlap, num_sub_cuboids_dim)
@@ -21,24 +19,31 @@ for i, sub_cuboid in enumerate(sub_cuboids):
     print(f"Sub-cuboid {i + 1}: Shape: {sub_cuboid.shape}")
 # print(sub_cuboids[3])
 for i, start_coord in enumerate(start_coords):
-    print(f"Sub-cuboid {i+1}: Start Coordinates: {start_coord}") 
+    print(f"Sub-cuboid {i+1}: Start Coordinates: {start_coord}")
 
 for i, end_coord in enumerate(end_coords):
     print(f"Sub-cuboid {i+1}: End Coordinates: {end_coord}")
 
 start_dict = conv_to_dict(start_coords)
-end_dict = conv_to_dict(end_coords) 
+end_dict = conv_to_dict(end_coords)
 
 adjacency_dict = check_for_adjacency(start_dict)
 
-cube = create_cuboid(start_dict, end_dict, (132,132,90))
+cube = create_cuboid(start_dict, end_dict, (145, 174, 145))
 
-np.save("D:/Research/Harvard/OverlappingTesselations/outputs/verification_outputs/cube_1.npy", cube)
+np.save("/data/pnl/home/ag1666/coeff_dir/CuboidTesselation/outputs/verification_outputs/cube_1.npy", cube)
 
 print("Numpy File Saved")
 
 nii_file = nib.Nifti1Image(cube, affine=np.eye(4))
 
-nib.save(nii_file, "D:/Research/Harvard/OverlappingTesselations/outputs/nifti_outputs/cube_1.nii")
+nib.save(nii_file, "/data/pnl/home/ag1666/coeff_dir/CuboidTesselation/outputs/nifti_outputs/cube_1.nii")
 
 print("NIfTI File Saved")
+
+#Generate Masks
+
+masks = create_binary_masks(start_dict, end_dict, (145, 174, 145))
+np.save("/data/pnl/home/ag1666/coeff_dir/CuboidTesselation/outputs/verification_outputs/masks_1.npy", masks)
+
+print("Masks Saved")
